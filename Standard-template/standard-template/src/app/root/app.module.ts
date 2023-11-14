3c7ba4c4c10b84a7a66cbc43793145c7ea4565cf
Jithin Raveendran <jithinjjsr@gmail.com>
2023-11-14 23:43:01 +0530

    mat table, bootstrap navbar, header, footer, home

--- Standard-template/standard-template/src/app/root/app.module.ts
+++ Standard-template/standard-template/src/app/root/app.module.ts
@@ -7,17 +7,28 @@ import { LayoutComponent } from './container/layout/layout.component';
 import { AppService } from './service/app.service';
 import { CoreModule } from '../core/core.module';
 import { HttpClientModule } from '@angular/common/http';
-
+import { HeaderComponent } from './container/header/header.component';
+import { FooterComponent } from './container/footer/footer.component';
+import { HomeComponent } from './container/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 @NgModule({
   declarations: [
     AppComponent,
-    LayoutComponent
+    LayoutComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     CoreModule.forRoot(),
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+     MatPaginatorModule
   ],
   providers: [
     AppService
--- Standard-template/standard-template/src/app/root/service/app.service.ts
+++ Standard-template/standard-template/src/app/root/service/app.service.ts
@@ -1,10 +1,11 @@
+import { HttpClient } from "@angular/common/http";
 import { Inject, Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 import { AuthService } from "src/app/providers/auth/auth.service";
 
 @Injectable()
 export class AppService {
-    constructor(@Inject(AuthService) private readonly authService: AuthService) {}
+    constructor(@Inject(AuthService) private readonly authService: AuthService, private http: HttpClient) {}
 
     /**
      * Function to return the observable state of the logged in status of the user.
@@ -12,5 +13,8 @@ export class AppService {
      */
     isUserAuthenticated (): Observable<boolean> {
         return this.authService.isAuthenticated();
+    }
+    getCustomers(): Observable<any>{
+        return this.http.get('https://localhost:7235/WeatherForecast/customers');
     }
 }
